be657f58d73dc952d7e54e0644eaa3a851313044
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-09-30 18:42:14 +0300

    success extracting user details

--- cards_dev/website/src/app/app.component.ts
+++ cards_dev/website/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'cards_fe';
   session_guid = null;
 
   api_access_jwt: Map<string, string> | null = null
-  user_logged_in = false
+  user_details: any = null
 
   onLogin(token: Map<string, string>) {
     this.api_access_jwt = token
-    this.user_logged_in = true
+    this.user_details = jwt_decode(this.api_access_jwt.get("id_token")!)
   }
 }
