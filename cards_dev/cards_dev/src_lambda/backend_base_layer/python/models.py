19135197fb1d3c2449917b883011475b8dd250da
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-10-05 23:12:34 +0300

    some untested refactors to models

--- cards_dev/cards_dev/src_lambda/backend_base_layer/python/models.py
+++ cards_dev/cards_dev/src_lambda/backend_base_layer/python/models.py
@@ -1,34 +1,31 @@
-from dataclasses import dataclass
+from abc import abstractclassmethod
+from dataclasses import dataclass, asdict, is_dataclass
 from enum import Enum
+from mimetypes import init
 from typing import Iterable, Mapping
 import json
 
-@dataclass
-class Player:
-    pass
+class GameDataClass:
+    def __init__(self) -> None:
+        assert is_dataclass(self)
+
+    def to_dict(self) -> dict:
+        return asdict(self)
 
 @dataclass
-class GameSession:
+class Player(GameDataClass):
+    name: str
+    callback_url: str
+
+@dataclass
+class GameSession(GameDataClass):
     class Phase(Enum):
         Enrollment="enrollment"
         Inquiry="inquiry"
         Applaud="applaud"
 
-    id: str
+    session_id: str  # must be the key in dynamodb sessions table
     phase: Phase
     round: int
+    coordinator_callback_url: str
     players_ids: Iterable[str]
-
-    def __init__(self, id: str, phase: Phase, round: int, players_ids: Iterable[str]) -> None:
-        self.id = id
-        self.phase = phase
-        self.round = round
-        self.players_ids = players_ids
-
-    def to_dict(self) -> dict:
-        return {
-            "session_id": self.id,  # must be the key in dynamodb sessions table
-            "phase": self.phase.value,
-            "round": self.round,
-            "players_ids": list(self.players_ids)
-        }
