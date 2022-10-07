94c4a7460cecc5a9cdb38b847d5e9e88bc1187e2
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-10-07 19:07:13 +0300

    implemented some data conversions

--- cards_dev/cards_dev/src_lambda/backend_base_layer/python/models.py
+++ cards_dev/cards_dev/src_lambda/backend_base_layer/python/models.py
@@ -1,41 +1,56 @@
-from dataclasses import dataclass, asdict, is_dataclass
+from dataclasses import dataclass, asdict
 from enum import Enum
-from typing import Iterable, Union
+from typing import Iterable, List, Union
+
+
+class Phase(Enum):
+        Enrollment="enrollment"
+        InProgress="inprogress"
+        Complete="complete"
 
 class GameDataClass:
-    def __init__(self) -> None:
-        assert is_dataclass(self)
-
     def to_dict(self) -> dict:
         return asdict(self)
+
+    def to_dynamodb_object(self, fluff_attributes: List[str], extra_attributes: dict) -> dict:
+        object_dict: dict = self.to_dict()
+        for attr in fluff_attributes:
+            object_dict.pop(attr)
+        object_dict.update(extra_attributes)
+        return object_dict
+
 
 @dataclass
 class Player(GameDataClass):
     name: str
     callback_url: str
 
+
+##
+## When changing property names, keep in mind the table key names (they need to be identical)
+##
+
 @dataclass
 class GameRound(GameDataClass):
-    class Phase(Enum):
-        Open="open"
-        Closed="closed"
-
-    phase: Phase
     session_id: str
+    round: int
     winner_id: Union[str,  None]
     question_card_text: str
     answer_cards_suggested: Iterable[str]
     winning_answer_index: int=0
 
+    def to_dynamodb_object(self) -> dict:
+         return super().to_dynamodb_object([], {})
+
+
 @dataclass
 class GameSession(GameDataClass):
-    class Phase(Enum):
-        Enrollment="enrollment"
-        InProgress="inprogress"
-        Finished="complete"
-
-    session_id: str  # must be the key in dynamodb sessions table
+    session_id: str
     phase: Phase
-    round: int
     coordinator_callback_url: str
     players_ids: Iterable[str]
+    active_round: Union[GameRound, None]
+    recent_rounds: List[GameRound]
+
+    def to_dynamodb_object(self) -> dict:
+         return super().to_dynamodb_object(["active_round", "recent_rounds"], {"round": 0})
