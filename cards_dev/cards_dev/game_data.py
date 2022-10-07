2d338d0b0060e14a1532f2515ffb688a51682470
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-10-07 22:47:56 +0300

    added card table and bucket to infra

--- cards_dev/cards_dev/game_data.py
+++ cards_dev/cards_dev/game_data.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack,
     RemovalPolicy,
     aws_dynamodb as dyndb,
+    aws_s3 as s3
 )
 from constructs import Construct
 from cards_dev.back_end import CardsBackend
@@ -14,9 +15,11 @@ class CardsGameData(Stack):
         retain_on_deletion = False
         self.__data_resource_removal_policy: RemovalPolicy = RemovalPolicy.RETAIN if retain_on_deletion else RemovalPolicy.DESTROY
         self.__create_session_table()
+        self.__create_cards_table()
+        self.__create_session_archive()
 
     def __create_session_table(self) -> None:
-        self.__session_data = dyndb.Table(self, "session_data", 
+        self.__session_data = dyndb.Table(self, "dev_session_data", 
             table_name="dev_session_data",
             removal_policy=self.__data_resource_removal_policy,
             partition_key= dyndb.Attribute(name="session_id", type=dyndb.AttributeType.STRING),
@@ -24,3 +27,20 @@ class CardsGameData(Stack):
         )
         for function in self.__backend.lambdas:
             self.__session_data.grant_read_write_data(function)
+
+    def __create_cards_table(self) -> None:
+        self.__cards_data = dyndb.Table(self, "dev_cards_data", 
+            table_name="dev_cards_data",
+            removal_policy=self.__data_resource_removal_policy,
+            partition_key= dyndb.Attribute(name="text", type=dyndb.AttributeType.STRING)
+        )
+        for function in self.__backend.lambdas:
+            self.__session_data.grant_read_write_data(function)
+
+    def __create_session_archive(self) -> None:
+        self.__session_archive = s3.Bucket(self, "dev_session_archive",
+            bucket_name="dev.session-archive",
+            versioned=False,
+            removal_policy=self.__data_resource_removal_policy,
+            auto_delete_objects=True
+        )
