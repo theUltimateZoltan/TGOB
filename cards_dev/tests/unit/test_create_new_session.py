f4a5c2359b802008b1fcce93fdcf434f96a5f948
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-10-08 11:55:52 +0300

    better mocking

--- cards_dev/tests/unit/test_create_new_session.py
+++ cards_dev/tests/unit/test_create_new_session.py
@@ -12,7 +12,7 @@ from backend_base_layer import GameData
 def session_table() -> Table:
     with mock_dynamodb():
         dynamodb = boto3.resource('dynamodb')
-        yield dynamodb.create_table(
+        mock_session_table = dynamodb.create_table(
             TableName='dev_session_data',
             KeySchema=[
                 {
@@ -36,6 +36,9 @@ def session_table() -> Table:
             ],
             BillingMode='PAY_PER_REQUEST'
         )
+        GameData.session_table = mock_session_table
+        yield
+
 
 @pytest.fixture
 def arbitrary_game_session() -> GameSession:
@@ -44,7 +47,6 @@ def arbitrary_game_session() -> GameSession:
 def test_generated_id_unique(session_table: Table, arbitrary_game_session: GameSession):
     # Keep in mind the race condition not being tested here. See https://advancedweb.hu/how-to-properly-implement-unique-constraints-in-dynamodb/
     with patch("cards_dev.src_lambda.create_new_session.create_new_session.randomword_generator") as randomword_generator_mock:
-        GameData.session_table = session_table
         number_of_words_in_sequence = 2
         randomword_generator_mock.word.side_effect = (
             [arbitrary_game_session.session_id]+
@@ -53,14 +55,13 @@ def test_generated_id_unique(session_table: Table, arbitrary_game_session: GameS
             [""] * (number_of_words_in_sequence-1)
         )
 
-        session_table.put_item(Item=arbitrary_game_session.to_dynamodb_object())
+        GameData.session_table.put_item(Item=arbitrary_game_session.to_dynamodb_object())
         create_new_session.lambda_handler({}, {})
 
         assert GameData.get_session(arbitrary_game_session.session_id)
         assert GameData.get_session("Anotherid")  
 
 def test_simple_session_creation(session_table):
-    GameData.session_table = session_table
     response = create_new_session.lambda_handler({}, {})
     session_id: str = json.loads(response.get("body")).get("session_id")
     assert GameData.get_session(session_id)
