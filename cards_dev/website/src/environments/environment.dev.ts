dbcfd18d4e3977577e817e8960b7c7970f28c0bb
Elad Levy <elad.levy2@mail.huji.ac.il>
2022-10-09 11:05:13 +0300

    join is implemented in backend

--- cards_dev/website/src/environments/environment.dev.ts
+++ cards_dev/website/src/environments/environment.dev.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: false,
-  backend_api_url: "https://api.devcards.eladlevy.click",
+  rest_api_url: "https://restapi.devcards.eladlevy.click",
+  websocket_api_url: "https://wsapi.devcards.eladlevy.click",
   website_url: "devcards.eladlevy.click",
-  cognito_user_pool_client_id: "4k9ppkrbhj153128nbr282q6p3",
-  cognito_user_pool_id: "us-west-2_zd1xUQuZy",
+  cognito_user_pool_client_id: "4v5j6c1js03miui3lq8d434phq",
+  cognito_user_pool_id: "us-west-2_6HcZs2kZ7",
   aws_region: "us-west-2",
 };
